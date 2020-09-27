dfdca314cf5366ed64919fcca2053afc53a64862
lufefe <jumbaayabonga@gmail.com>
2020-09-27 15:30:06 +0200

    css cleanup

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,13 +14,13 @@ function Cards() {
               src='https://images.generated.photos/vg5a0p99H1OW2vcHaU0GSAbYPMycPErzAQEPzNHLjE4/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA2MTE3NTAuanBn.jpg'
               text='Mike Larry'
               label='Founder and CEO'
-              path='/services'
+
             />
             <CardItem
               src='https://images.generated.photos/QEJrIALGbpSLKrnJdWeKcU_E2WMBRZETNlFqGIafjl8/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAyODI1MjUuanBn.jpg'
               text='Cynthia Beck'
               label='CIO'
-              path='/services'
+      
             />
           </ul>
           <ul className='cards__items'>
@@ -28,19 +28,28 @@ function Cards() {
               src='https://images.generated.photos/I6Qb-wlzRGzfddb_gNjHZh3ezcVQT7JERMcLFIxEwI8/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA3NjI0MjIuanBn.jpg'
               text='Nosipho Swazi'
               label='CFO'
-              path='/services'
+          
             />
-            <CardItem
-              src='https://images.generated.photos/v3G0DCFTYj4G4SpXxzGCt5b7A-i5FXbUYmQ570h4pG0/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAxODc1ODQuanBn.jpg'
-              text='Mitch Planko'
-              label='Strategic Advisor'
-              path='/products'
-            />
+           
             <CardItem
               src='https://images.generated.photos/TVl_gxuCpWDY4ASXh0P_hKlYqW-hoB3RY2xTjnMXuEQ/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA2ODUxMTIuanBn.jpg'
               text='Gloria Finn'
+              label='COO'
+             
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://images.generated.photos/A0Xl0uYeDh2-ff007-LzAFDKu1UJSg22vYK4TRqfigk/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4Njc5OTguanBn.jpg'
+              text='Chantelle Louwren'
               label='Non-Executive Director'
-              path='/sign-up'
+              
+            />
+           
+            <CardItem
+              src='https://images.generated.photos/v3G0DCFTYj4G4SpXxzGCt5b7A-i5FXbUYmQ570h4pG0/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAxODc1ODQuanBn.jpg'
+              text='Mitch Planko'
+              label='CTO'
             />
           </ul>
         </div>
@@ -48,5 +57,7 @@ function Cards() {
     </div>
   );
 }
+
+
 
 export default Cards;
--- src/components/Vision.js
+++ src/components/Vision.js
@@ -8,7 +8,7 @@ function Vision() {
             <div className="vision-content">
                 <div className="main-col-left">
                     <h1>Vision</h1>
-                    <p>Our imagination is that of limitless possibilities. We beleive that the future we want we can create.
+                    <p>Our vision is that of limitless possibilities. We believe that the future we want, we can create.
                         We envision a future where technology is used safely to capture and process our data, doing the work for us in every way possible.
                         Technology should be merely a means to a better society, providing us with the necessary products.
                     </p>
